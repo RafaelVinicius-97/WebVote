25e139a51d886930207d573a75c4b5d2bf27ac06
Rafael Vinicius <rafael.silva@aevo.com.br>
2021-08-21 10:49:38 -0300

    subindo_projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebVote.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
